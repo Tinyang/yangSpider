46c716628f0f54c0f811dd20e45e6d4403e9e2ed
Tinyang <1251307117@qq.com>
2021-08-20 16:33:00 +0800

    douban spider

---
+++ src/main/java/com/yang/DouBanPageProcessor.java
@@ -0,0 +1,61 @@
+package com.yang;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.HtmlNode;
+import us.codecraft.webmagic.selector.Selectable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DouBanPageProcessor implements PageProcessor {
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+    @Override
+    public void process(Page page) {
+        HtmlNode htmlNode = (HtmlNode) page.getHtml().xpath("//*[@id=\"billboard\"]/div[2]/table/tbody//a");
+        List<Selectable> htmlNodeList = htmlNode.nodes();
+        List<DoubanObject> list = new ArrayList<>();
+        for (Selectable selectable: htmlNodeList) {
+            String name = selectable.xpath("//a/text()").toString();
+            list.add(new DoubanObject(name));
+        }
+        page.putField("douban", list);
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new DouBanPageProcessor()).addUrl("https://movie.douban.com/").thread(5).run();
+
+    }
+
+}
+
+class DoubanObject{
+    private String name;
+
+    public DoubanObject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "DoubanObject{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
